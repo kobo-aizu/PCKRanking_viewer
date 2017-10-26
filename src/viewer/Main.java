ec73d4e96a02ce1c8aff4533e38a008f81f32314
fussy113 <s1220080@gmail.com>
2017-10-26 16:37:44 +0900

    viewの簡易作成

--- src/viewer/Main.java
+++ src/viewer/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Ranking");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
     }
 
