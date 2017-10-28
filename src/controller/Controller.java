00eceed24c6399ebba2e3a80c98be80862545414
fussy113 <s1220080@gmail.com>
2017-10-28 14:46:25 +0900

    ControllerとMainにわけた

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,18 +6,35 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-
-
 
 public class Controller implements Initializable{
 
+    private int[] num;
+    private int max_team = 28;
+    @FXML
+    VBox ranking;
+    @FXML
+    Label testLabel;
 
+    public void Load(){
+        num = new int[max_team];
+        for(int i=0;i<max_team;i++){
+            num[i]= new Random().nextInt(max_team);
+            System.out.println(num[i]);
+        }
+        Write();
+    }
+
+    public void Write(){
+        ranking.getChildren().clear();
+        for(int i = 0;i<max_team;i++){
+            ranking.getChildren().add(new Label(String.valueOf(num[i])));
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
-
+        Load();
     }
 }
--- src/viewer/Main.java
+++ src/viewer/Main.java
@@ -1,27 +1,12 @@
 package viewer;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.Random;
-import java.util.ResourceBundle;
-
-public class Main extends Application implements Initializable{
-    private int[] num;
-    private int max_team = 10;
-    @FXML
-    VBox ranking;
-
-    @FXML
-    Label testLabel;
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -31,28 +16,8 @@ public class Main extends Application implements Initializable{
         primaryStage.show();
     }
 
-    public void Load(){
-        num = new int[max_team];
-        for(int i=0;i<max_team;i++){
-            num[i]= new Random().nextInt(max_team);
-            System.out.println(num[i]);
-        }
-        Write();
-    }
-
-    public void Write(){
-        ranking.getChildren().clear();
-        for(int i = 0;i<max_team;i++){
-            ranking.getChildren().add(new Label(String.valueOf(num[i])));
-        }
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources){
-        Load();
-    }
 }
